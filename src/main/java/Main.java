a298515cbad8f03b1c29ccadeb398010017c8240
Roman Bulinski <aboandrus@gmail.com>
2019-06-20 16:45:57 +0200

    Refactor code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,22 +2,19 @@ public class Main {
 
     public static final String generateShape(int n) {
 
-        String result = "";
-
         if(n==0){
-            result ="";
+            return "";
         }else{
+            String result = "";
             for(int j=0; j<n; j++){
                 String temostring = "";
                 for(int i=0; i<n; i++){
                     temostring = temostring+"+";
                 }
-                temostring = temostring+"\n";
-                result = result + temostring;
+                result = result + temostring+"\n";
             }
-            result=result.substring(0,result.length()-1);
+            return result.substring(0,result.length()-1);
         }
-        return result;
     }
 
 
