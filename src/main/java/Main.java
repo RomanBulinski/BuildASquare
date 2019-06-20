d0143bc95458acd376fb2845f2aad83da3de0c0c
Roman Bulinski <aboandrus@gmail.com>
2019-06-20 16:41:46 +0200

    Added empty string case

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,16 +4,20 @@ public class Main {
 
         String result = "";
 
-        for(int j=0; j<n; j++){
-            String temostring = "";
-            for(int i=0; i<n; i++){
-                temostring = temostring+"+";
+        if(n==0){
+            result ="";
+        }else{
+            for(int j=0; j<n; j++){
+                String temostring = "";
+                for(int i=0; i<n; i++){
+                    temostring = temostring+"+";
+                }
+                temostring = temostring+"\n";
+                result = result + temostring;
             }
-            temostring = temostring+"\n";
-            result = result + temostring;
+            result=result.substring(0,result.length()-1);
         }
-
-        return result.substring(0,result.length()-1);
+        return result;
     }
 
 
